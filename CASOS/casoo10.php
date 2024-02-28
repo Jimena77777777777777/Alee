880c748a68107709b3b4c339511932d38f8864e1
Alejandra <202100355d@utea.edu.pe>
2024-02-28 00:27:57 -0500

    E

--- CASOS/casoo10.php
+++ CASOS/casoo10.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Simulador de préstamos</title>
+  <title>CASO 10 </title>
   <style>
     body {
             display: flex;
